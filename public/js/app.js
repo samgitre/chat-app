8cc6db68cd8f25a5d7ca844f4d2eb60c6f12b2a8
Sam <samgitrepos@gmail.com>
2017-03-19 00:07:23 +0700

    Conneting between browsers

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,18 @@ socket.on('connect', function () {
   console.log('User connected');
 });
 
-
 socket.on('message', function(message) {
   console.log('new message');
   console.log(message.text);
+  jQuery('.messages').append('<p>' + message.text + '</p>')
+});
+
+var $form = jQuery('#messager-form');
+$form.on('submit', function(event){
+  event.preventDefault();
+  var $message = $form.find('input[name=messenger]');
+    socket.emit('message', {
+      text:$message.val()});
+      $message.val('');
+      $message.focus();
 });
