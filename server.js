93bd00b17d6d340e54706a39e7641b1c4fdd63e1
Sam <samgitrepos@gmail.com>
2017-03-18 15:34:35 +0700

    Setting up of socket.io

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var Port = process.env.PORT || 3000;
+var io = require('socket.io')(http);
 
 
 
@@ -11,6 +12,10 @@ var Port = process.env.PORT || 3000;
 
 
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function() {
+  console.log('user connected');
+});
 
 http.listen(Port, function() {
   console.log('app running at :' + Port);
