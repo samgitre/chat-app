e7a7cc23fccbce6d242a3cd5f93244d764642122
Sam <samgitrepos@gmail.com>
2017-03-19 09:47:53 +0700

    time stamp added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var Port = process.env.PORT || 3000;
 var io = require('socket.io')(http);
+var moment = require('moment');
 
 
 app.use(express.static(__dirname + '/public'));
@@ -12,12 +13,14 @@ io.on('connection', function(socket) {
 
   socket.on('message', function(message) {
     console.log('message recieved' + message.text);
+    message.timestamp = moment.valueOf();
     io.emit('message', message);
     //socket.broadcast.emit('message', message);
   });
 
   socket.emit('message',{
-    text : 'Welcome to nitechsy group forum'
+    text : 'Welcome to nitechsy group forum',
+    timestamp: moment.valueOf()
   });
 });
 
